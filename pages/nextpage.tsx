f89d7bfe53d51abb18fc1db4553bd588264ef30e
Aniket Singh <31aniket.singh@gmail.com>
2021-07-10 11:31:21 +0530

    added component styling

--- pages/nextpage.tsx
+++ pages/nextpage.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
-
+import styles from './component.module.css'
 const nextpage = () => {
     return (
-        <div>
+        <div className={styles.main}>
             <h2>I'm Second Page</h2>
         </div>
     )
