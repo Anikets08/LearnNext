254f183ed07e52e48871409b146fa8d8318e1ff9
Aniket Singh <31aniket.singh@gmail.com>
2021-07-10 11:18:22 +0530

    added jsx styling (similar to the styled component)

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,14 @@ import router, { useRouter } from 'next/router'
 export default function Home() {
   return (
    <div className="home">
-     <h1>Hello World</h1>
+     <h1 className="title">Hello World</h1>
+     <style jsx>{
+       `
+       .title{
+         color:grey;
+       }
+       `
+     }</style>
      <button onClick={()=>router.push({pathname:'/nextpage'})}>Click Me</button>
      <button onClick={()=>router.reload()}>Refresh Page</button>
    </div>
